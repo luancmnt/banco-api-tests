e5cd96ce10e3bdf4fc4380c0df1015db6d6482f0
nascimentoluana <167343668+nascimentoluana@users.noreply.github.com>
2026-03-24 21:16:08 +0000

    Adicionando testes de transferência

--- test/login.test.js
+++ test/login.test.js
@@ -1,10 +1,9 @@
 const request = require('supertest');
 const { expect } = require('chai');
 
-
 describe('Login', () => {
     describe('POST /login', () => {
-        it('Deve retornar 200 com token em string quando usar credenciais válidas', async () => {
+        it('Should respond with 200 and a token string when using valid credentials', async () => {
             const response = await request('http://localhost:3000')
                 .post('/login')
                 .set('Content-Type', 'application/json')
---
+++ test/transferencia.test.js
@@ -0,0 +1,58 @@
+const request = require('supertest');
+const { expect } = require('chai');
+
+describe('Transfers', () => {
+    describe('POST /transferencias', () => {
+        it('Should respond with 201 when the transfer amount is equal to or greater than 10.00', async () => {
+            // get token 
+            const responseLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                        'username': 'julio.lima', 
+                        'senha': '123456'
+                })
+
+            const token = responseLogin.body.token
+            
+            const response = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}` )
+                .send({
+                    'contaOrigem': '1',
+                    'contaDestino': '2',
+                    'valor': '11',
+                    'token': ""
+                })
+
+            expect(response.status).to.equal(201);
+        })
+
+        it('Should respond with 422 when the transfer amount is less than 10.00', async () => {
+            // get token 
+            const responseLogin = await request('http://localhost:3000')
+                .post('/login')
+                .set('Content-Type', 'application/json')
+                .send({
+                        'username': 'julio.lima', 
+                        'senha': '123456'
+                })
+
+            const token = responseLogin.body.token
+            
+            const response = await request('http://localhost:3000')
+                .post('/transferencias')
+                .set('Content-Type', 'application/json')
+                .set('Authorization', `Bearer ${token}` )
+                .send({
+                    'contaOrigem': '1',
+                    'contaDestino': '2',
+                    'valor': '9',
+                    'token': ""
+                })
+
+            expect(response.status).to.equal(422);
+        })
+    })
+})
